6a4485cd19f0252aa7588a57596c87b824fe78a5
Tarushi <107392283+tarushi16@users.noreply.github.com>
2023-09-12 12:15:56 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class p1 {
+    public static void main(String[] args) {
+        System.out.println("enter 2 nos");
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
